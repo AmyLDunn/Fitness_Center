3c8ee968fc7b7242356f56786472b1275d03514f
AmyLDunn <ldunn084@uottawa.ca>
2021-10-15 14:02:05 -0400

    Added the documentation to all of the methods and the class itself.

--- app/src/main/java/com/example/fitnesscenter/DBHelper.java
+++ app/src/main/java/com/example/fitnesscenter/DBHelper.java
@@ -43,6 +43,10 @@ public class DBHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, 1);
     }
 
+    /**
+     * This method runs the first time the database is created and no more
+     * @param db is the database to check for
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE accounts (id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, accountType INTEGER NOT NULL DEFAULT 2)");
@@ -50,6 +54,12 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE classtypes (id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE, description TEXT)");
     }
 
+    /**
+     * This method upgrades the database if any changes are made
+     * @param db is the database to be upgraded
+     * @param oldVersion is the version number
+     * @param newVersion is the new version number
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS accounts");
@@ -57,6 +67,10 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+    /**
+     * This returns all of the entries in the accounts table
+     * @return an ArrayList<Account> of all Account objects created from the table
+     */
     public ArrayList<Account> getAllAccounts(){
         ArrayList<Account> accounts = new ArrayList<Account>();
         SQLiteDatabase db = this.getReadableDatabase();
@@ -75,6 +89,13 @@ public class DBHelper extends SQLiteOpenHelper {
         return accounts;
     }
 
+    /**
+     * This gets a specific account from the database
+     * @param username is the username of the account
+     * @param password is the password of the account
+     * @return an Account object if the username-password pair is found in one entry
+     *         or null if that pair was not found
+     */
     public Account getAccount(String username, String password) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = db.rawQuery("SELECT * FROM accounts WHERE username = "+username+" AND password = "+password+"", null);
@@ -88,6 +109,11 @@ public class DBHelper extends SQLiteOpenHelper {
         return thisAccount;
     }
 
+    /**
+     * This checks if a username is already taken for an account
+     * @param username to be searched for
+     * @return true if the username is being used or false if it's not in the database
+     */
     public boolean accountExists(String username){
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = db.rawQuery("SELECT * FROM accounts WHERE username = "+username+"", null);
@@ -103,11 +129,20 @@ public class DBHelper extends SQLiteOpenHelper {
         return false;
     }
 
+    /**
+     * Deletes an account from the database
+     * @param id of the account to be deleted
+     * @return true if the account was successfully deleted
+     */
     public int deleteAccount(int id){
         SQLiteDatabase db = this.getWritableDatabase();
         return db.delete("accounts", "id = ?", new String[] { Integer.toString(id)});
     }
 
+    /**
+     * This returns all of the entries in the classtypes table
+     * @return an ArrayList<ClassType> of all ClassType objects created from the table
+     */
     public ArrayList<ClassType> getAllClassTypes(){
         ArrayList<ClassType> classtypes = new ArrayList<ClassType>();
         SQLiteDatabase db = this.getReadableDatabase();
@@ -125,6 +160,12 @@ public class DBHelper extends SQLiteOpenHelper {
         return classtypes;
     }
 
+    /**
+     * Adds a new class type to the classtypes table if the name isn't present
+     * @param name of the class type to be added
+     * @param description of the class type to be added
+     * @return true if the class type was successfully added
+     */
     public boolean addClassType(String name, String description) {
         if ( classTypeExists(name) ) {
             return false;
@@ -137,6 +178,11 @@ public class DBHelper extends SQLiteOpenHelper {
         return true;
     }
 
+    /**
+     * This checks if a name is already taken for a class type
+     * @param name to be searched for
+     * @return true if the name is being used or false if it's not in the database
+     */
     public boolean classTypeExists(String name) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = db.rawQuery("SELECT * FROM classtypes WHERE name = "+name+"", null);
@@ -152,6 +198,13 @@ public class DBHelper extends SQLiteOpenHelper {
         return false;
     }
 
+    /**
+     * Updates the name and/or description of a class type if the new name is available
+     * @param id of the class type to be updated
+     * @param name new name of the class type
+     * @param description new description of the class type
+     * @return true if the update was successful
+     */
     public boolean updateClassType(int id, String name, String description) {
         if ( classTypeExists(name) ) {
             return false;
@@ -164,6 +217,11 @@ public class DBHelper extends SQLiteOpenHelper {
         return true;
     }
 
+    /**
+     * Deletes a classtype from the database
+     * @param id of the classtype to be deleted
+     * @return true if the classtype was successfully deleted
+     */
     public int deleteClassType(int id){
         SQLiteDatabase db = this.getWritableDatabase();
         return db.delete("classtypes", "id = ?", new String[] { Integer.toString(id)});
