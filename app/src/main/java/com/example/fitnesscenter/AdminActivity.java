47a608e3e2f60099f54a27491f0769b5ed495438
AmyLDunn <ldunn084@uottawa.ca>
2021-10-18 13:53:32 -0400

    Split the welcome screen into a tabbed screen. Right now all user types can see the admin version of the tabs, but we can fix that. Only have two tabs so far as well, but this is working, so I'm posting it now.

---
+++ app/src/main/java/com/example/fitnesscenter/AdminActivity.java
@@ -0,0 +1,41 @@
+package com.example.fitnesscenter;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.fitnesscenter.ui.main.SectionsPagerAdapter;
+import com.example.fitnesscenter.databinding.ActivityMainBinding;
+
+public class AdminActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Account userAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle data = getIntent().getExtras();
+        userAccount = (Account) data.getParcelable("USER_ACCOUNT");
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+
+    }
+}
